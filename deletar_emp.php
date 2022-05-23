fdf958a6ac0a57f7151f59a3b04eec6e6a823c37
Juscelino Campos <celino3x@gmail.com>
2022-05-23 11:09:48 -0300

    Criação de página de exclusão de dados

--- deletar_emp.php
+++ deletar_emp.php
@@ -1,4 +1,18 @@
 <?php
         include('header_.php');
         include('_conexao.php');
+
+        $id = $_GET['id'];
+
+        $query = "DELETE FROM `cad_empresa` WHERE id = $id";
+        $deletar = mysqli_query($conexao,$query);
+?>
+
+<div class="container">
+        <h3>Empresa Deletada</h3>
+        <a href="consulta.php" class="btn btn-sm btn-warning"><i class="fas fa-arrow-left"></i> Voltar</a>
+</div>
+
+<?php
+        include('footer_.php');
 ?>
